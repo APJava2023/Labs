1f349cafc22e6a88ffd5e71de1ee1957ad88684f
Alex <alexanderchapman2007@gmail.com>
2022-12-20 09:47:03 -0500

    added main method

---
+++ ReviewSentiment/Alex/Main.java
@@ -0,0 +1,10 @@
+package Alex;
+
+class Main {
+  
+    public static void main(String[] args) {
+      // Activity 1: Call the sentimentVal method in Review with a word like "terrible" and print out the result
+      System.out.println(Review.sentimentVal("aaron"));
+      System.out.println(Review.totalSentiment("AP-CSA-Consumer-Review-Lab-Student-Files/26WestReview.txt"));
+    }
+  }
